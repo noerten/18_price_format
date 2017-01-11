416fc6a0974a560ed415283b4796cdf539608a08
noerten <darkstria@gmail.com>
2017-01-11 23:38:44 +0300

    true float support

--- format_price.py
+++ format_price.py
@@ -7,21 +7,24 @@ def parse_price():
     return parser.parse_args()
 
 
-def prettify_int(int_price):
-    return '{:,}'.format(int_price).replace(",", " ")
+def prettify_price(price):
+    if int(price) == float(price):
+        decimals = 0
+    else:
+        decimals = 2
+    return '{0:,.{1}f}'.format(price, decimals).replace(",", " ")
 
 
 def format_price(price):
     try:
-        int_price = int(price)
+        int(price)
     except ValueError:
         return None
     else:
-        return prettify_int(int_price)
-
+        return prettify_price(price)
 
 if __name__ == '__main__':
     user_price = parse_price().price
     print(format_price(user_price))
 
-    
+
--- tests.py
+++ tests.py
@@ -6,8 +6,13 @@ from format_price import format_price
 class TestFormatPrice(unittest.TestCase):
 
     def test_float(self):
-        self.assertEqual(format_price(77775.00), '77 775')
+        self.assertEqual(format_price(77775.666666), '77 775.67')
 
+    def test_float_to_int(self):
+        self.assertEqual(format_price(77775.00000), '77 775')
+
+    def test_int(self):
+        self.assertEqual(format_price(77775), '77 775')
 
     def test_invalid_value(self):
         self.assertIsNone(format_price('seven'))
